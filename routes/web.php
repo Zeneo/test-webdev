00a6c2784c9c3055ef86ddd62f1221e1d6766fa0
PauliusNavickas <paulius.zee@gmail.com>
2023-11-30 20:54:07 +0200

    Readme

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('instructions');
 });
+Route::get('/users',  [\App\Http\Controllers\UserController::class, 'index']);
